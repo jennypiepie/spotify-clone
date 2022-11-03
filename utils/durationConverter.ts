b2b164e03a396520ae8c88362cabad36b7acbc26
派派 <2416655925@qq.com>
2022-11-03 23:05:04 +0800

    time bug

--- utils/durationConverter.ts
+++ utils/durationConverter.ts
@@ -1,5 +1,5 @@
 export const convertDuration = (duration: number) => {
-    const minutes = Math.floor(duration / 6000)
+    const minutes = Math.floor(duration / 60000)
     const seconds = ((duration % 60000) / 1000).toFixed(0)
     return minutes + ':' + (Number(seconds) < 10 ? '0' : '') + seconds
 }
